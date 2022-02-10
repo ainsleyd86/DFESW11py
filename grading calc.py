9b03d779bb055326fc46f4a82170aff83f917e0f
ainsleyd86 <ainsley.dobbs@outlook.com>
2022-02-10 17:14:40 +0000

    adding grading calc code

---
+++ grading calc.py
@@ -0,0 +1,53 @@
+#devs_money = 100
+#dev_can_play_smash = False
+
+
+#if devs_money > 10 and dev_can_play_smash:
+#    print("Dev enters a smash tournament!")
+#elif devs_money < 10 and dev_can_play_smash:
+ #   print("Dev is too poor to enter")
+#else:
+#    print("Dev just can't play smash")
+
+
+# Mark = int(input("what grade did i recieve "))
+# if Mark >=85:
+#     print("distinction")
+# elif Mark==65 or Mark >65:
+#     print ("pass")
+# elif Mark <85:
+#     print ("pass")
+# else Mark <65:
+#     print ("fail")
+
+# Mark = int(input("what grade did i recieve "))
+# def determine_grade(scores):
+#     if scores >= 90 and <= 100:
+#         return 'A'
+#     elif scores >= 80 and <= 89:
+#         return 'B'
+#     elif scores >= 70 and <= 79:
+#         return 'C'
+#     elif scores >= 60 and <= 69:
+#         return 'D'
+#     elif scores >= 50 and <= 59:
+#         return 'E'
+#     else:
+#         return 'F'
+
+scores = int(input("what grade did i recieve "))
+
+if scores >95:
+    print ("Distinction")
+elif scores >= 90 and scores <= 95:
+    print ('A')
+elif scores >= 80:
+    print('B')
+elif scores >= 70:
+    print ('C')
+elif scores >= 60:
+    print ('D')
+elif scores >= 50:
+    print ('E')
+else:
+    print ('F')
